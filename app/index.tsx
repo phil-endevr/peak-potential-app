d96549007e42f1ff14716f61d91b23fe0b6f91d7
phil-endevr <phil.endevr@gmail.com>
2025-09-23 00:39:43 +0100

    Clean up unused files

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: "https://peakpotential.world/app/sign-in" }} />
+      <StatusBar style="light" />
+
+      <SafeAreaView style={styles.safeArea}>
+        <WebView source={{ uri: "https://peakpotential.world/app/sign-in" }} />
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,5 +19,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#000000",
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#000000",
   },
 });
