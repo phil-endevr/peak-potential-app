8d8ce69c67f457a4aadf101e6094f10461944cba
phil-endevr <phil.endevr@gmail.com>
2026-03-04 22:00:31 +0000

    Remove revenuecat logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,83 +1,15 @@
-import AppleSignUpModal from "@/components/appleSignup/AppleSignUpModal";
 import WebviewApp from "@/components/WebviewApp";
-import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import {
-  Modal,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import Purchases, { LOG_LEVEL } from "react-native-purchases";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [isSettingsPage, setIsSettingsPage] = useState<boolean>(false);
-
-  useEffect(() => {
-    Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
-
-    if (Platform.OS === "ios") {
-      Purchases.configure({
-        apiKey: Constants.expoConfig?.extra?.revenueCatIosKey,
-      });
-    }
-  }, []);
-
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <SafeAreaView style={styles.safeArea} edges={["left", "right", "bottom"]}>
-        <WebviewApp
-          setIsLoggedIn={setIsLoggedIn}
-          setIsSettingsPage={setIsSettingsPage}
-        />
-        {Platform.OS === "ios" && !isLoggedIn && (
-          <View
-            style={{
-              position: "absolute",
-              bottom: 20,
-              left: 0,
-              right: 0,
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ color: "white" }}>Don&apos;t have an account?</Text>
-            <TouchableOpacity onPress={() => setShowModal(true)}>
-              <Text
-                style={{
-                  color: "#d3d3d3",
-                  textDecorationLine: "underline",
-                  marginTop: 5,
-                }}
-              >
-                Register here
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-
-        <Modal
-          animationType="slide"
-          visible={showModal}
-          onRequestClose={() => setShowModal(false)}
-        >
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "#000000",
-            }}
-          >
-            <AppleSignUpModal onClose={() => setShowModal(false)} />
-          </View>
-        </Modal>
+        <WebviewApp />
       </SafeAreaView>
     </View>
   );
